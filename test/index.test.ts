f1767999fdefb89d722ed25eb748b525b5995868
alemagio <alessandro.magionami@gmail.com>
2020-10-20 19:55:21 +0200

    test: added structure for tests

---
+++ test/index.test.ts
@@ -0,0 +1,9 @@
+import { test } from 'tap'
+import { fastify } from 'fastify'
+
+const postgraphile = require('../src')
+
+test('should add a reply decorator', async t => {
+  t.plan(1)
+  t.todo('')
+})
